5bbc98aeb4483529fdd61101271f89db35de6583
William Soares <pedro.williamsoares2022@gmail.com>
2026-03-24 14:34:28 -0300

    style(api): ajustar indentação do código

--- front-literalura/src/services/api.tsx
+++ front-literalura/src/services/api.tsx
@@ -2,14 +2,23 @@ import axios from 'axios';
 
 // Criando uma instância (serve como o garçom personalizado)
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+  baseURL: 'http://localhost:8080',
 
-    timeout: 5000,
-    
-    // Tempo limite de espera (5 segundos). Se o servidor demorar mais que isso, ele cancela.
-    headers: {
-        'Content-Type': 'application/json',
-    }
+  timeout: 5000,
+
+  // Tempo limite de espera (5 segundos). Se o servidor demorar mais que isso, ele cancela.
+  headers: {
+    'Content-Type': 'application/json',
+  }
+
+  // Interceptor para tratar respostas e erros
+  // api.interceptors.response.use(
+  //   (response: AxiosResponse) => response, // Se der certo, só passa adiante
+  //   (error: AxiosError) => {
+  //     alert("Houve um erro de conexão com o servidor!");
+  //     return Promise.reject(error);
+  //   }
+  // );
 });
 
 export default api;
